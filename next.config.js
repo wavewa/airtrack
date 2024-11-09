a76a8420138d17c36d818800f6c05080dda26cad
Walter <walterverdino@yahoo.it>
2024-11-09 14:59:21 +0100

    add deploy github

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: isProd ? '/airtrack/' : '',
+  basePath: isProd ? '/airtrack' : '',
+  output: 'export',
   experimental: {
     appDir: true,
   },
   images: {
+    unoptimized: true, // Disable default image optimization
     domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
   },
 };
